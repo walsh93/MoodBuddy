22e27ad7b68f37aa12cb46bbd0002ff92dbf5d0c
Marilyn Wood <wood152@purdue.edu>
2018-11-27 00:34:26 -0500

    THIS IS ALL GARBAGE

--- mood-buddy/server.js
+++ mood-buddy/server.js
@@ -1,67 +1,45 @@
-// Get dependencies
+//Constants
+const distDir = __dirname + "/dist/mood-buddy";
+
+//Dependancies
 const express = require('express');
 const path = require('path');
-const http = require('http');
 const bodyParser = require('body-parser');
-const sessions = require('client-sessions');
-var admin = require('firebase-admin');
-const firebaseConfig = __dirname + "/mood-buddy-firebase-adminsdk-u7omy-5241f4a0c9.json"
-var serviceAccount = require(firebaseConfig);
-
-// Get our API routes
-const api = require('./server/routes/api');
 
 const app = express();
-
-//Initialize Firebase and Firestore
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: 'https://<Mood-Buddy>.firebaseio.com'
-  });
 
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//Client sessions
-app.use(sessions({
-    cookieName: 'moodBuddySession',
-    secret: 'largeunguessablestring',
-    duration: 30 * 60 * 1000,
-    cookie: {
-        path: '/',
-        ephemeral: true,
-        httpOnly: true,
-    }
-}));
+// Point static path to /local/path/to/CheckedIn/checkedIn/dist
+app.use(express.static(__dirname));
+app.use(express.static(distDir));
 
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
-
-//Routes
-const signupRouter = require(`${routesDir}signup`)
-
-// Set our api routes
-app.use('/api', api);
-app.use(c.signup-page, signupRouter);
-
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+// GET requests sent when routing to the page
+app.get('/', function(request, response) {
+  response.sendFile(path.join(distDir, "index.html"));
+  console.log("GET /");
+  console.log(request.headers);
+  console.log("\n");
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
+app.get('/welcome', function(request, response) {
+    response.sendFile(path.join(distDir, "index.html"));
+    console.log("GET /");
+    console.log(request.headers);
+    console.log("\n");
+});
 
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
+app.get('/signup', function(request, response) {
+    response.sendFile(path.join(distDir, "index.html"));
+    console.log("GET /");
+    console.log(request.headers);
+    console.log("\n");
+});
 
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+// Get port from environment
+const port = process.env.PORT || 4200;
+
+app.listen(port);
+console.log("Server started on port " + port);
--- mood-buddy/src/app/app.module.ts
+++ mood-buddy/src/app/app.module.ts
@@ -1,26 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule,Routes, Router} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
-import { SignInPageComponent } from './sign-in-page/sign-in-page.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { MoodLogComponent } from './mood-log/mood-log.component';
-import { JournalComponent } from './journal/journal.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {path:'',redirectTo:'welcome', pathMatch:'full'},
+  {path:'welcome',component:WelcomePageComponent},
+  {path:'signup',component:SignupPageComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomePageComponent,
-    SignInPageComponent,
-    DashboardComponent,
-    MoodLogComponent,
-    JournalComponent,
-    SignupPageComponent
+    SignupPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing:true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
