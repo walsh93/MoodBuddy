a34e5670bc1b18403a81bdc03797ae550c930ba1
walsh93 <walsh93@purdue.edu>
2018-12-03 18:33:29 -0500

    Beginning of stuff to show cards of journals. Doesn't work yet, but committing so I don't lose it

--- mood-buddy/src/app/journal/journal.component.ts
+++ mood-buddy/src/app/journal/journal.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DataHandlerService } from '../data-handler.service';
+import { User } from '../users';
 @Component({
   selector: 'app-journal',
   templateUrl: './journal.component.html',
   styleUrls: ['./journal.component.css']
 })
 export class JournalComponent implements OnInit {
+  user: User = new User();
+  userID: string;
+  mood: string;
+  intensity: string;
+  journal: string;
+  activity: string;
+  date: string;
 
-  constructor() { }
+
+  constructor(private dataHandlerService: DataHandlerService) { }
 
   ngOnInit() {
+
+    this.dataHandlerService.getUserData().then((moodBuddySession)=>{
+      this.userID = moodBuddySession.userId;
+      const logs = moodBuddySession.logs;
+      let i = 0;
+      for(i = 0; i < logs.length; i++){
+       this.mood = logs[i].mood;
+       this.intensity = logs[i].intensity;
+       this.date = logs[i].date;
+       this.activity = logs[i].activity;
+       this.journal = logs[i].journal;
+      }
+    });
   }
 
 }
